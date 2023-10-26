383305a0189b9c7128d7924c233603ccffc7d6e6
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-10-26 11:52:04 -0400

    Agregando el contexto e inyectandolo en el program

---
+++ Server/DAL/Context.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+public class Context :DbContext
+{
+    public DbSet<Prioridades> Prioridades {get; set;}
+
+    public Context(DbContextOptions<Context> options) : base(options) { }
+}
