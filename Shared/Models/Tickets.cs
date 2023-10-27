b04d217261bc0652ee7252d326cb248284aeaebb
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-10-27 11:49:49 -0400

    Creando el modelo Tickets

---
+++ Shared/Models/Tickets.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Tickets
+{
+    [Key]
+    public int TicketId {get; set;}
+
+    [Required(ErrorMessage ="Debe de insertar una Fecha")]
+    public DateTime Fecha {get; set;} = DateTime.Now;
+
+    [Required(ErrorMessage ="El campo {0} es obligatorio")]
+    [StringLength(40, ErrorMessage ="Ha insertado mas de {1} caracteres")]
+    [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage ="El campo {0} solo debe de contener caracteres alfabeticos")]
+    public string SolicitadoPor {get; set;} = string.Empty;
+
+    [Required(ErrorMessage ="El campo {0} es obligatorio")]
+    [StringLength(70, MinimumLength =5, ErrorMessage ="El campo {0} debe de tener entre {2} a {1} caracteres")]
+    public string Asunto {get; set;} = string.Empty;
+
+    [Required(ErrorMessage ="El campo {0} es obligatorio")]
+    [StringLength(80, MinimumLength = 5, ErrorMessage ="El campo {0} debe de tener entre {2} a {1} caracteres")]
+    public string Descripcion {get; set;} = string.Empty;
+}
