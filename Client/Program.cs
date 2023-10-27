e86b40ae96b0b5770b13f6ec89163917b3d99820
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-10-26 21:51:13 -0400

    Agregando NotificationService

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Migracion_Tarea1_Hasta_Tarea4.Client;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<NotificationService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
