ac965e7e1c3f958ca39d81d916ae0c9d89425d84
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-10-26 23:25:28 -0400

    Agregando el DbSet del modelo Clientes

--- Server/DAL/Context.cs
+++ Server/DAL/Context.cs
@@ -4,5 +4,7 @@ public class Context :DbContext
 {
     public DbSet<Prioridades> Prioridades {get; set;}
 
+    public DbSet<Clientes> Clientes {get; set;}
+
     public Context(DbContextOptions<Context> options) : base(options) { }
 }
